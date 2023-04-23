27e1ed3388f32cded2ddff53f8f1d30ea07e7874
thelaziestfox <127044222+thelaziestfox@users.noreply.github.com>
2023-04-24 00:41:17 +0300

    Add files via upload

---
+++ 4-2-triangle.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as RP
+import time
+
+def dec2bin(value):
+    return [int(elem) for elem in bin(value)[2:].zfill(8)]
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+RP.setmode(RP.BCM)
+RP.setmode(dac, RP.OUT)
+
+try:
+    pass
