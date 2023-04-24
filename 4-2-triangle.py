913ee9caa1e64c0500b2f7c1b5a85288f536507b
Angelina Kholopova <kholopova.angelina@phystech.edu>
2023-04-24 14:14:21 +0300

    final

--- 4-2-triangle.py
+++ 4-2-triangle.py
@@ -11,3 +11,18 @@ RP.setmode(dac, RP.OUT)
 try:
     while True:
         a = input()
+        if a == 'q':
+            break
+        elif a.isdigit() and int(a) % 1 == 0 and 0 <= int(a) <= 255:
+            RP.output(dac, dec2bin(int(a)))
+            print("{:.4f".format((int(a)/256)*3.3))
+        else:
+            for i in range(256):
+                RP.output(dac, dec2bin(i))
+                time.sleep(int(a)/512)
+            for i in range(255, -1, -1):
+                RP.output(dac, dec2bin(i))
+                time.sleep(int(a)/512)
+finally:
+    RP.output(dac, 0)
+    RP.cleanup()
